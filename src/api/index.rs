8cbd6c92068e9f77458ca1fd0ac2bd82d5e0504a
simoh8 <simomutu8@gmail.com>
2025-11-22 20:22:38 +0300

    first commit last  serchs

--- src/api/index.rs
+++ src/api/index.rs
@@ -1,14 +1,16 @@
-use vercel_runtime::{run, Body, Error, Request, Response};
-use http::{StatusCode};
+use http::{Response, StatusCode};
+use vercel_runtime::{run, Body, Error, Request, Response as VercelResponse};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     run(handler).await
 }
 
-pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
-    Ok(Response::builder()
+pub async fn handler(_req: Request) -> Result<VercelResponse<Body>, Error> {
+    let response = Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "text/html")
-        .body(Body::Text("M-Pesa STK Push API".into()))?)
+        .body(Body::Text("M-Pesa API is running".to_string()))?;
+        
+    Ok(response)
 }
